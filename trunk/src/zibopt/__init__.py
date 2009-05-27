a8abd6cdc5d2a190feb9d5f51adf5cc92d054115
ryan@chenoneil.com <ryan@chenoneil.com@1deb0140-4a77-11de-b99e-5975e9e2e9d3>
2009-05-27 19:55:47 +0000

    forgot __init__.py

---
+++ trunk/src/zibopt/__init__.py
@@ -0,0 +1,17 @@
+# python-zibopt: Python interface to ZIB Optimization Suite
+# Copyright (C) 2009  Ryan J. O'Neil
+# http://code.google.com/p/pyython-zibopt/
+
+'''
+python-zibopt: Python interface to ZIB Optimization Suite
+Copyright (C) 2009  Ryan J. O'Neil
+http://code.google.com/p/pyython-zibopt/
+
+This software is released under the GPL 3.0.
+'''
+
+from zibopt import scip
+
+__version__ = '0.1'
+__all__ = 'scip',
+
