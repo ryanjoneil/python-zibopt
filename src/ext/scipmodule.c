d4de02cf28518888fdd90bbd3143720d9c78da45
ryan@chenoneil.com <ryan@chenoneil.com@1deb0140-4a77-11de-b99e-5975e9e2e9d3>
2009-06-19 15:38:24 +0000

    spacing cleanup

--- src/ext/scipmodule.c
+++ src/ext/scipmodule.c
@@ -16,8 +16,15 @@ static PyObject *solver_new(PyTypeObject *type, PyObject *args, PyObject *kwds) 
         // Default plugins, heuristics, etc
         PY_SCIP_CALL(error, NULL, SCIPincludeDefaultPlugins(self->scip));
         
-        // Create an empty problem
-        // TODO: what are all these NULLs for?
+        // SCIPcreateProb Arguments:
+        // scip         SCIP data structure
+        // name         name of problem
+        // probdelorig  callback to free original problem data
+        // probtrans    callback to create transformed problem
+        // probdeltrans callback to free that transformed problem
+        // probinitsol  callback to create initial solution
+        // probexitsol  callback to free initial solution
+        // probdata     initial problem data (vars & constraints)
         PY_SCIP_CALL(error, NULL, 
             SCIPcreateProb(self->scip, "python-zibobt", NULL, NULL, NULL, NULL, NULL, NULL)
         );
@@ -80,47 +87,47 @@ static PyMethodDef solver_methods[] = {
 
 static PyTypeObject solver_type = {
     PyObject_HEAD_INIT(NULL)
-    0,                         /*ob_size*/
-    "_scip.solver",             /*tp_name*/
-    sizeof(solver),             /*tp_basicsize*/
-    0,                         /*tp_itemsize*/
-    (destructor) solver_dealloc, /*tp_dealloc*/
-    0,                         /*tp_print*/
-    0,                         /*tp_getattr*/
-    0,                         /*tp_setattr*/
-    0,                         /*tp_compare*/
-    0,                         /*tp_repr*/
-    0,                         /*tp_as_number*/
-    0,                         /*tp_as_sequence*/
-    0,                         /*tp_as_mapping*/
-    0,                         /*tp_hash */
-    0,                         /*tp_call*/
-    0,                         /*tp_str*/
-    0,                         /*tp_getattro*/
-    0,                         /*tp_setattro*/
-    0,                         /*tp_as_buffer*/
-    Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE, /*tp_flags*/
-    "SCIP solver objects",           /* tp_doc */
-    0,		               /* tp_traverse */
-    0,		               /* tp_clear */
-    0,		               /* tp_richcompare */
-    0,		               /* tp_weaklistoffset */
-    0,		               /* tp_iter */
-    0,		               /* tp_iternext */
-    solver_methods,             /* tp_methods */
-    0,                         /* tp_members */
-    0,                         /* tp_getset */
-    0,                         /* tp_base */
-    0,                         /* tp_dict */
-    0,                         /* tp_descr_get */
-    0,                         /* tp_descr_set */
-    0,                         /* tp_dictoffset */
+    0,                           /* ob_size */
+    "_scip.solver",              /* tp_name */
+    sizeof(solver),              /* tp_basicsize */
+    0,                           /* tp_itemsize */
+    (destructor) solver_dealloc, /* tp_dealloc */
+    0,                           /* tp_print */
+    0,                           /* tp_getattr */
+    0,                           /* tp_setattr */
+    0,                           /* tp_compare */
+    0,                           /* tp_repr */
+    0,                           /* tp_as_number */
+    0,                           /* tp_as_sequence */
+    0,                           /* tp_as_mapping */
+    0,                           /* tp_hash */
+    0,                           /* tp_call */
+    0,                           /* tp_str */
+    0,                           /* tp_getattro */
+    0,                           /* tp_setattro */
+    0,                           /* tp_as_buffer */
+    Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE, /* tp_flags */
+    "SCIP solver objects",       /* tp_doc */
+    0,                           /* tp_traverse */
+    0,                           /* tp_clear */
+    0,                           /* tp_richcompare */
+    0,                           /* tp_weaklistoffset */
+    0,                           /* tp_iter */
+    0,                           /* tp_iternext */
+    solver_methods,              /* tp_methods */
+    0,                           /* tp_members */
+    0,                           /* tp_getset */
+    0,                           /* tp_base */
+    0,                           /* tp_dict */
+    0,                           /* tp_descr_get */
+    0,                           /* tp_descr_set */
+    0,                           /* tp_dictoffset */
     (initproc) solver_init,      /* tp_init */
-    0,                         /* tp_alloc */
-    solver_new,                 /* tp_new */
+    0,                           /* tp_alloc */
+    solver_new,                  /* tp_new */
 };
 
-#ifndef PyMODINIT_FUNC	/* declarations for DLL import/export */
+#ifndef PyMODINIT_FUNC    /* declarations for DLL import/export */
 #define PyMODINIT_FUNC void
 #endif
 PyMODINIT_FUNC init_scip(void) {
--- src/ext/solnmodule.c
+++ src/ext/solnmodule.c
@@ -67,47 +67,47 @@ static PyMethodDef solution_methods[] = {
 
 static PyTypeObject solution_type = {
     PyObject_HEAD_INIT(NULL)
-    0,                         /*ob_size*/
-    "_soln.solution",             /*tp_name*/
-    sizeof(solution),             /*tp_basicsize*/
-    0,                         /*tp_itemsize*/
-    (destructor) solution_dealloc, /*tp_dealloc*/
-    0,                         /*tp_print*/
-    0,                         /*tp_getattr*/
-    0,                         /*tp_setattr*/
-    0,                         /*tp_compare*/
-    0,                         /*tp_repr*/
-    0,                         /*tp_as_number*/
-    0,                         /*tp_as_sequence*/
-    0,                         /*tp_as_mapping*/
-    0,                         /*tp_hash */
-    0,                         /*tp_call*/
-    0,                         /*tp_str*/
-    0,                         /*tp_getattro*/
-    0,                         /*tp_setattro*/
-    0,                         /*tp_as_buffer*/
-    Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE, /*tp_flags*/
-    "SCIP solution objects",           /* tp_doc */
-    0,		               /* tp_traverse */
-    0,		               /* tp_clear */
-    0,		               /* tp_richcompare */
-    0,		               /* tp_weaklistoffset */
-    0,		               /* tp_iter */
-    0,		               /* tp_iternext */
-    solution_methods,          /* tp_methods */
-    solution_members,          /* tp_members */
-    0,                         /* tp_getset */
-    0,                         /* tp_base */
-    0,                         /* tp_dict */
-    0,                         /* tp_descr_get */
-    0,                         /* tp_descr_set */
-    0,                         /* tp_dictoffset */
+    0,                             /* ob_size */
+    "_soln.solution",              /* tp_name */
+    sizeof(solution),              /* tp_basicsize */
+    0,                             /* tp_itemsize */
+    (destructor) solution_dealloc, /* tp_dealloc */
+    0,                             /* tp_print */
+    0,                             /* tp_getattr */
+    0,                             /* tp_setattr */
+    0,                             /* tp_compare */
+    0,                             /* tp_repr */
+    0,                             /* tp_as_number */
+    0,                             /* tp_as_sequence */
+    0,                             /* tp_as_mapping */
+    0,                             /* tp_hash */
+    0,                             /* tp_call */
+    0,                             /* tp_str */
+    0,                             /* tp_getattro */
+    0,                             /* tp_setattro */
+    0,                             /* tp_as_buffer */
+    Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE, /* tp_flags */
+    "SCIP solution objects",       /* tp_doc */
+    0,                             /* tp_traverse */
+    0,                             /* tp_clear */
+    0,                             /* tp_richcompare */
+    0,                             /* tp_weaklistoffset */
+    0,                             /* tp_iter */
+    0,                             /* tp_iternext */
+    solution_methods,              /* tp_methods */
+    solution_members,              /* tp_members */
+    0,                             /* tp_getset */
+    0,                             /* tp_base */
+    0,                             /* tp_dict */
+    0,                             /* tp_descr_get */
+    0,                             /* tp_descr_set */
+    0,                             /* tp_dictoffset */
     (initproc) solution_init,      /* tp_init */
-    0,                         /* tp_alloc */
-    0 ,                 /* tp_new */
+    0,                             /* tp_alloc */
+    0 ,                            /* tp_new */
 };
 
-#ifndef PyMODINIT_FUNC	/* declarations for DLL import/export */
+#ifndef PyMODINIT_FUNC    /* declarations for DLL import/export */
 #define PyMODINIT_FUNC void
 #endif
 PyMODINIT_FUNC init_soln(void) {
