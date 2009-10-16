45d61c906169a1194a9ed0bc8361d6dd808eb347
ryan@chenoneil.com <ryan@chenoneil.com@1deb0140-4a77-11de-b99e-5975e9e2e9d3>
2009-10-16 14:36:44 +0000

    shuffling unit tests

---
+++ trunk/src/tests/settings.py
@@ -0,0 +1,27 @@
+from zibopt import scip, _branch
+import unittest
+
+class ScipSettingsTest(unittest.TestCase):
+    def testBadBranchingRule(self):
+        '''Test loading a branching rule that doesn't exist'''
+        solver = scip.solver()
+        self.assertRaises(scip.BranchingRuleError, _branch.branching_rule, solver, 'NOSUCHRULE')
+    
+    def testLoadBranchingRuleNames(self):
+        '''Loads names of branching rules'''
+        solver = scip.solver()
+        rules = solver.branching_rules()
+        self.assertTrue(rules)
+        self.assertEqual(set(rules), set(solver.branching.keys()))
+        
+    def testSetBranchingPriority(self):
+        '''Sets branching priority'''
+        solver = scip.solver()
+        for n, b in solver.branching.items():
+            x = b.priority
+            b.priority = x + 1
+            self.assertEqual(x+1, b.priority)            
+        
+if __name__ == '__main__':
+    unittest.main()
+
