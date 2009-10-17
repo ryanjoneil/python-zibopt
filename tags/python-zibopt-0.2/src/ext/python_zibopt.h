f2c48f65a172c0359ea77ff39f17071315d4a8a2
ryan@chenoneil.com <ryan@chenoneil.com@1deb0140-4a77-11de-b99e-5975e9e2e9d3>
2009-10-17 17:22:41 +0000

    starting 0.3 branch

---
+++ tags/python-zibopt-0.2/src/ext/python_zibopt.h
@@ -0,0 +1,153 @@
+#ifndef PYTHON_ZIBOPT_H
+#define PYTHON_ZIBOPT_H
+
+#include <Python.h>
+#include <stdbool.h>
+#include <string.h>
+
+#include <scip/clock.h>
+#include <scip/scip.h>
+#include <scip/scipdefplugins.h>
+#include <scip/misc.h>
+#include <scip/prob.h>
+#include <scip/sol.h>
+#include <scip/struct_branch.h>
+#include <scip/struct_conflict.h>
+#include <scip/struct_sepa.h>
+#include <scip/struct_stat.h>
+#include "structmember.h"
+
+#define SOLVER_TYPE_NAME "solver"
+#define VARIABLE_TYPE_NAME "_vars.variable"
+
+// These are from set.c in SCIP code
+#define SCIP_DEFAULT_LIMIT_TIME 1e+20 /**< maximal time in seconds to run */
+#define SCIP_DEFAULT_LIMIT_GAP    0.0 /**< solving stops, if the gap is below the given value */
+#define SCIP_DEFAULT_LIMIT_ABSGAP 0.0 /**< solving stops, if the absolute difference between primal and dual
+                                       *   bound reaches this value */
+
+typedef struct {
+    PyObject_HEAD
+    SCIP_VAR *variable;
+    SCIP *scip;
+    struct variable *next; // linked list next
+} variable;
+
+typedef struct {
+    PyObject_HEAD
+    SCIP_CONS *constraint;
+    SCIP *scip;
+    struct constraint *next; // linked list next
+} constraint;
+
+typedef struct {
+    PyObject_HEAD
+    SCIP *scip;
+    variable   *first_var;   // linked list head
+    constraint *first_cons;  // linked list head
+} solver;
+
+typedef struct {
+    PyObject_HEAD
+    SCIP_SOL *solution;
+    SCIP *scip;
+    double objective; // objective value
+    bool optimal;     // solution status flags
+    bool infeasible;
+    bool unbounded;
+    bool inforunbd;
+} solution;
+
+typedef struct {
+    PyObject_HEAD
+    SCIP_BRANCHRULE *branch;
+    SCIP *scip;
+} branching_rule;
+
+typedef struct {
+    PyObject_HEAD
+    SCIP_CONFLICTHDLR *conflict;
+    SCIP *scip;
+} conflict;
+
+typedef struct {
+    PyObject_HEAD
+    SCIP_SEPA *sepa;
+    SCIP *scip;
+} separator;
+
+static void PyScipSetError(PyObject *error_type, SCIP_RETCODE err_code) {
+    switch(err_code) {
+        case SCIP_OKAY:
+            PyErr_SetString(error_type, "normal termination");
+            break;
+        case SCIP_ERROR:
+            PyErr_SetString(error_type, "unspecified error");
+            break;
+        case SCIP_NOMEMORY:
+            PyErr_SetString(error_type, "insufficient memory error");
+            break;
+        case SCIP_READERROR:
+            PyErr_SetString(error_type, "file read error");
+            break;
+        case SCIP_WRITEERROR:
+            PyErr_SetString(error_type, "file write error");
+            break;
+        case SCIP_NOFILE:
+            PyErr_SetString(error_type, "file not found error");
+            break;
+        case SCIP_FILECREATEERROR:
+            PyErr_SetString(error_type, "cannot create file");
+            break;
+        case SCIP_LPERROR:
+            PyErr_SetString(error_type, "error in LP solver");
+            break;
+        case SCIP_NOPROBLEM:
+            PyErr_SetString(error_type, "no problem exists");
+            break;
+        case SCIP_INVALIDCALL:
+            PyErr_SetString(error_type, "method cannot be called at this time in solution process");
+            break;
+        case SCIP_INVALIDDATA:
+            PyErr_SetString(error_type, "method cannot be called with this type of data");
+            break;
+        case SCIP_INVALIDRESULT:
+            PyErr_SetString(error_type, "method returned an invalid result code");
+            break;
+        case SCIP_PLUGINNOTFOUND:
+            PyErr_SetString(error_type, "a required plugin was not found");
+            break;
+        case SCIP_PARAMETERUNKNOWN:
+            PyErr_SetString(error_type, "the parameter with the given name was not found");
+            break;
+        case SCIP_PARAMETERWRONGTYPE:
+            PyErr_SetString(error_type, "the parameter is not of the expected type");
+            break;
+        case SCIP_PARAMETERWRONGVAL:
+            PyErr_SetString(error_type, "the value is invalid for the given parameter");
+            break;
+        case SCIP_KEYALREADYEXISTING:
+            PyErr_SetString(error_type, "the given key is already existing in table");
+            break;
+        case SCIP_PARSEERROR:
+            PyErr_SetString(error_type, "invalid input given to the parser");
+            break;
+        case SCIP_MAXDEPTHLEVEL:
+            PyErr_SetString(error_type, "maximal branching depth level exceeded");
+            break;
+        default:
+            PyErr_SetString(error_type, "an unregocnized error occurred in SCIP");
+    }
+}
+
+#define PY_SCIP_CALL(error_type, fail_code, code) \
+    do { \
+        SCIP_RETCODE _restat_; \
+        if ((_restat_ = (code)) != SCIP_OKAY) { \
+            PyScipSetError(error_type, _restat_); \
+            return fail_code; \
+        } \
+    } while (FALSE);
+
+#endif
+
---
+++ tags/python-zibopt-0.2/src/ext/varsmodule.c
@@ -0,0 +1,146 @@
+#include "python_zibopt.h"
+
+static PyObject *error;
+
+/*****************************************************************************/
+/* PYTHON TYPE METHODS                                                       */
+/*****************************************************************************/
+static int variable_init(variable *self, PyObject *args, PyObject *kwds) {
+    static char *argnames[] = {"solver", "coefficient", "vartype", "lower", "upper", NULL};
+    PyObject *s;     // solver Python object
+    solver *solv;    // solver C object
+    double c;        // coefficient
+    int t;           // integer / binary / continuous
+    double lhs, rhs; // lhs <= a'x <= rhs
+    
+    t = SCIP_VARTYPE_CONTINUOUS;
+
+    // SCIPinfinity requires self->scip, so we have to parse the args twice
+    if (!PyArg_ParseTuple(args, "Od|idd", &s, &c, &t, &lhs, &rhs))
+        return -1;
+
+    // Check solver type in the best way we seem to have available
+    if (strcmp(s->ob_type->tp_name, SOLVER_TYPE_NAME)) {
+        PyErr_SetString(error, "invalid solver type");
+        return -1;
+    }
+    
+    solv = (solver *) s;
+    self->scip = solv->scip;
+    
+    lhs = -SCIPinfinity(self->scip);
+    rhs = SCIPinfinity(self->scip);
+
+    // This time is just to get the upper and lower bounds out    
+    if (!PyArg_ParseTupleAndKeywords(args, kwds, "Od|idd", argnames, &s, &c, &t, &lhs, &rhs))
+        return -1;
+
+    // Variable type
+    if (t != SCIP_VARTYPE_BINARY && t != SCIP_VARTYPE_INTEGER && t != SCIP_VARTYPE_IMPLINT) {
+        t = SCIP_VARTYPE_CONTINUOUS;
+    } else if (t == SCIP_VARTYPE_BINARY) {
+        if (lhs < 0)
+            lhs = 0;
+        if (rhs > 1)
+            rhs = 1;
+    }
+    
+    // SCIPcreateVar Arguments:
+    // scip         SCIP data structure
+    // var          pointer to variable object
+    // name         name of variable, or NULL for automatic name creation
+    // lb           lower bound of variable
+    // ub           upper bound of variable
+    // obj          objective function value
+    // vartype      type of variable
+    // initial      should var's column be present in the initial root LP?
+    // removable    is var's column removable from the LP?
+    // vardata      user data for this specific variable 
+    PY_SCIP_CALL(error, -1, 
+        SCIPcreateVar(self->scip, &self->variable, NULL, lhs, rhs, c, t,
+            TRUE, FALSE, NULL, NULL, NULL, NULL)
+    );
+            
+    PY_SCIP_CALL(error, -1, SCIPaddVar(self->scip, self->variable));
+
+    // Put new variable at head of linked list
+    self->next = solv->first_var;
+    solv->first_var = self;
+
+    return 0;
+}
+
+static void variable_dealloc(variable *self) {
+    self->ob_type->tp_free((PyObject *) self);
+}
+
+/*****************************************************************************/
+/* MODULE INITIALIZATION                                                     */
+/*****************************************************************************/
+static PyMethodDef variable_methods[] = {
+    {NULL} /* Sentinel */
+};
+
+static PyTypeObject variable_type = {
+    PyObject_HEAD_INIT(NULL)
+    0,                             /* ob_size */
+    "_vars.variable",              /* tp_name */
+    sizeof(variable),              /* tp_basicsize */
+    0,                             /* tp_itemsize */
+    (destructor) variable_dealloc, /* tp_dealloc */
+    0,                             /* tp_print */
+    0,                             /* tp_getattr */
+    0,                             /* tp_setattr */
+    0,                             /* tp_compare */
+    0,                             /* tp_repr */
+    0,                             /* tp_as_number */
+    0,                             /* tp_as_sequence */
+    0,                             /* tp_as_mapping */
+    0,                             /* tp_hash */
+    0,                             /* tp_call */
+    0,                             /* tp_str */
+    0,                             /* tp_getattro */
+    0,                             /* tp_setattro */
+    0,                             /* tp_as_buffer */
+    Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE, /* tp_flags */
+    "SCIP variable objects",       /* tp_doc */
+    0,                             /* tp_traverse */
+    0,                             /* tp_clear */
+    0,                             /* tp_richcompare */
+    0,                             /* tp_weaklistoffset */
+    0,                             /* tp_iter */
+    0,                             /* tp_iternext */
+    variable_methods,              /* tp_methods */
+    0,                             /* tp_members */
+    0,                             /* tp_getset */
+    0,                             /* tp_base */
+    0,                             /* tp_dict */
+    0,                             /* tp_descr_get */
+    0,                             /* tp_descr_set */
+    0,                             /* tp_dictoffset */
+    (initproc) variable_init,      /* tp_init */
+    0,                             /* tp_alloc */
+    0,                             /* tp_new */
+};
+
+#ifndef PyMODINIT_FUNC    /* declarations for DLL import/export */
+#define PyMODINIT_FUNC void
+#endif
+PyMODINIT_FUNC init_vars(void) {
+    PyObject* m;
+
+    variable_type.tp_new = PyType_GenericNew;
+    if (PyType_Ready(&variable_type) < 0)
+        return;
+
+    m = Py_InitModule3("_vars", variable_methods, "SCIP Variable");
+
+    Py_INCREF(&variable_type);
+    PyModule_AddObject(m, "variable", (PyObject *) &variable_type);
+
+    // Initialize exception type
+    error = PyErr_NewException("_vars.error", NULL, NULL);
+    Py_INCREF(error);
+    PyModule_AddObject(m, "error", error);
+}
+
