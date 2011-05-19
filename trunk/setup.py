cbdabfdee85b8eaf7c041fee65888adcfbb48202
ryanjoneil@gmail.com <ryanjoneil@gmail.com@1deb0140-4a77-11de-b99e-5975e9e2e9d3>
2011-05-19 14:12:51 +0000

    changing compile options to not use ipopt for the moment

--- trunk/setup.py
+++ trunk/setup.py
@@ -6,7 +6,7 @@ def zibopt_ext(name, c_file):
     return Extension('zibopt.%s' % name, 
         sources = ['src/ext/%s' % c_file], 
         libraries = [
-            'lpispx', 'objscip', 'scip', 'soplex', 'nlpi.ipopt', 'ipopt'
+            'lpispx', 'objscip', 'scip', 'soplex', 'nlpi', #'nlpi.ipopt', 'ipopt'
         ],
     )
 
