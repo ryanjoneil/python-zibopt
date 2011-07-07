30f0b20bf4632ac8440d88901fb7c6d0465e26f0
ryanjoneil@gmail.com <ryanjoneil@gmail.com@1deb0140-4a77-11de-b99e-5975e9e2e9d3>
2011-07-07 19:37:25 +0000

    tagging 0.7 release, starting 0.7.1 in trunk

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def zibopt_ext(name, c_file):
 
 setup (
     name         = 'python-zibopt',
-    version      = '0.7',
+    version      = '0.7.1',
     description  = 'ZIB Optimization Suite interface for python',
     author       = "Ryan J. O'Neil",
     author_email = 'ryanjoneil@gmail.com',
