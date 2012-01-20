3b9102ca5329c2eafc72cce899013981cb5dc9b2
ryanjoneil@gmail.com <ryanjoneil@gmail.com@1deb0140-4a77-11de-b99e-5975e9e2e9d3>
2012-01-20 21:58:17 +0000

    we have exception handling that works straight out of scip! finally! (first time evar)

--- branches/python-zibopt-0.8/src/zibopt/czibopt.pxd
+++ branches/python-zibopt-0.8/src/zibopt/czibopt.pxd
@@ -1,4 +1,9 @@
 cdef extern from "scip/scip.h":
+    ctypedef struct SCIP_MESSAGEHDLR:
+        void *messageerror
+    SCIP_MESSAGEHDLR *SCIPmessageGetHandler()
+    void SCIPmessageSetDefaultHandler()
+    
     ctypedef enum SCIP_RETCODE:
         pass
     SCIP_RETCODE SCIP_OKAY
@@ -8,5 +13,4 @@ cdef extern from "scip/scip.h":
 
     int SCIPcreate(SCIP **scip)
     int SCIPsolve(SCIP *scip)
-
 
--- branches/python-zibopt-0.8/src/zibopt/scip.pyx
+++ branches/python-zibopt-0.8/src/zibopt/scip.pyx
@@ -1,15 +1,29 @@
 cimport czibopt
 
+SCIP_ERROR_STR = ''
+
+cdef void PY_SCIP_CAPTURE_ERROR(void *a, void *b, char *c):
+    global SCIP_ERROR_STR
+    SCIP_ERROR_STR += c   
+
 def PY_SCIP_CALL(czibopt.SCIP_RETCODE retcode):
+    cdef czibopt.SCIP_MESSAGEHDLR *message_hdlr 
+    global SCIP_ERROR_STR
     if retcode != czibopt.SCIP_OKAY:
-        # TODO: better-ify this!
-        raise Exception('Error <%d> in SCIP' % retcode)
+        error = SCIP_ERROR_STR
+        SCIP_ERROR_STR = ''
+        raise Exception(error)
 
 def test():
+    cdef czibopt.SCIP_MESSAGEHDLR *message_hdlr 
     cdef czibopt.SCIP *scip
+    
+    message_hdlr = czibopt.SCIPmessageGetHandler()
+    message_hdlr.messageerror = <void *> PY_SCIP_CAPTURE_ERROR
+        
     # This should work
     PY_SCIP_CALL(czibopt.SCIPcreate(&scip))
-    
+
     # And this should raise an exception
     PY_SCIP_CALL(czibopt.SCIPsolve(scip))
 
